a6fb95f10e18c0aecf4b94dbb338bb7519930956
bookie <altashana@gmail.com>
2019-08-23 17:32:07 +0200

    adding fuzzy_cmp with memory block handling

--- tests/bilboquet.yara
+++ tests/bilboquet.yara
@@ -1,7 +1,14 @@
 import "pe"
 import "boo"
 
-rule hello_world{
+rule HOT_FUZZ{
     condition:
-        boo.fuzzy(0,50)
+        // boo.fuzzy(pe.sections[0].raw_data_offset, pe.sections[2].raw_data_offset - pe.sections[1].raw_data_offset - pe.sections[0].raw_data_offset)
+        
+        for any i in (0..pe.number_of_sections - 1):(
+            90 <= boo.fuzzy_cmp("48:rQ7BzU7BzJIBUF9OP9QzS3CIiNji2gJsoiOdvgz:r6Oil9+S3CosoFdC",boo.fuzzy(pe.sections[i].raw_data_offset, pe.sections[i].raw_data_size))
+        )
+
+        // boo.fuzzy_cmp("48:rQ7BzlTmWIBUF9OP9QzS3CIiNji2gJsoiOdvgz:r6lAl9+S3CosoFdC", "48:rQ7BzU7BzJIBUF9OP9QzS3CIiNji2gJsoiOdvgz:r6Oil9+S3CosoFdC")
+
 }
--- yara/libyara/modules/boo.c
+++ yara/libyara/modules/boo.c
@@ -7,36 +7,67 @@ define_function(fuzzy_hash)
 {
     YR_SCAN_CONTEXT *context = scan_context();
     YR_MEMORY_BLOCK* block = first_memory_block(context);
-
-    // printf("\nblock : %d\n", block->size);
     const uint8_t* block_data;
+
+    struct fuzzy_state *ctx = fuzzy_new();
 
     uint64_t offset = integer_argument(1);
     uint64_t length = integer_argument(2);
 
-    printf("offset : %d\n", offset);
-    printf("length : %d\n", length);
+    fuzzy_set_total_input_length(ctx, length);
+
+    char result[FUZZY_MAX_RESULT];
 
     foreach_memory_block(context->iterator, block)
     {
         block_data =  block->fetch_data(block);
+
         if (block_data != NULL)
         {
-            printf("%d", block->size);
+          if (length > block->size)
+          {
+            fuzzy_update(ctx, block_data + offset, block->size);
+            length = length - (block->size - offset);
+            offset = 0;
+          }
+          else
+          {
+            fuzzy_update(ctx, block_data + offset, length);
+          }
         }
         else
         {
-            printf("\nblock is empty..\n");
+          return_integer(0);
         }
-        
     }
+    fuzzy_digest(ctx, result, 0);
+    // printf("%s\n", result);
+    return_string(result);
+}
 
+
+define_function(fuzzy_cmp)
+{
+  YR_SCAN_CONTEXT *context = scan_context();
+  YR_MEMORY_BLOCK *block = first_memory_block(context);
+
+  char *first_fuzz = string_argument(1);
+  char *second_fuzz = string_argument(2);
+
+  int result;
+
+  result = fuzzy_compare(first_fuzz, second_fuzz);
+
+  // printf("fuzzy compare : %i\n", result);
+
+  return_integer(result);
 }
 
 
 begin_declarations;
 
     declare_function("fuzzy", "ii", "s", fuzzy_hash);
+    declare_function("fuzzy_cmp", "ss","i", fuzzy_cmp);
 
 end_declarations;
 
